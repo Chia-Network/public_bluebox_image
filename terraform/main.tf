e14cea2441b848d0afb4368d6fb8d3c213d8846e
Patrick Maslana <pmaslana3@gmail.com>
2021-08-24 13:27:09 -0700

    Fixed formatting and uncommented the lines for copying over the AMI to the rest the other five regions.

--- terraform/main.tf
+++ terraform/main.tf
@@ -2,25 +2,25 @@ provider "aws" {
   region = var.region1
 }
 
-//provider "aws" {
-//  alias  = "region2"
-//  region = var.region2
-//}
-//
-//provider "aws" {
-//  alias  = "region3"
-//  region = var.region3
-//}
-//
-//provider "aws" {
-//  alias  = "region4"
-//  region = var.region4
-//}
-//
-//provider "aws" {
-//  alias  = "region5"
-//  region = var.region5
-//}
+provider "aws" {
+  alias  = "region2"
+  region = var.region2
+}
+
+provider "aws" {
+  alias  = "region3"
+  region = var.region3
+}
+
+provider "aws" {
+  alias  = "region4"
+  region = var.region4
+}
+
+provider "aws" {
+  alias  = "region5"
+  region = var.region5
+}
 
 data "aws_ami" "ami" {
   most_recent = true
@@ -32,42 +32,42 @@ data "aws_ami" "ami" {
   }
 }
 
-//module "ami_copy_region2" {
-//  source            = "git::https://github.com/Chia-Network/terraform-modules.git//ami_copy"
-//  source_ami_id     = data.aws_ami.ami.id
-//  image_copy_name   = var.image_copy_name
-//  source_ami_region = var.region1
-//  providers         = {
-//    aws = aws.region2
-//  }
-//}
-//
-//module "ami_copy_region3" {
-//  source            = "git::https://github.com/Chia-Network/terraform-modules.git//ami_copy"
-//  source_ami_id     = data.aws_ami.ami.id
-//  image_copy_name   = var.image_copy_name
-//  source_ami_region = var.region1
-//  providers         = {
-//    aws = aws.region3
-//  }
-//}
-//
-//module "ami_copy_region4" {
-//  source            = "git::https://github.com/Chia-Network/terraform-modules.git//ami_copy"
-//  source_ami_id     = data.aws_ami.ami.id
-//  image_copy_name   = var.image_copy_name
-//  source_ami_region = var.region1
-//  providers         = {
-//    aws = aws.region4
-//  }
-//}
-//
-//module "ami_copy_region5" {
-//  source            = "git::https://github.com/Chia-Network/terraform-modules.git//ami_copy"
-//  source_ami_id     = data.aws_ami.ami.id
-//  image_copy_name   = var.image_copy_name
-//  source_ami_region = var.region1
-//  providers         = {
-//    aws = aws.region5
-//  }
-//}
+module "ami_copy_region2" {
+  source            = "git::https://github.com/Chia-Network/terraform-modules.git//ami_copy"
+  source_ami_id     = data.aws_ami.ami.id
+  image_copy_name   = var.image_copy_name
+  source_ami_region = var.region1
+  providers         = {
+    aws = aws.region2
+  }
+}
+
+module "ami_copy_region3" {
+  source            = "git::https://github.com/Chia-Network/terraform-modules.git//ami_copy"
+  source_ami_id     = data.aws_ami.ami.id
+  image_copy_name   = var.image_copy_name
+  source_ami_region = var.region1
+  providers         = {
+    aws = aws.region3
+  }
+}
+
+module "ami_copy_region4" {
+  source            = "git::https://github.com/Chia-Network/terraform-modules.git//ami_copy"
+  source_ami_id     = data.aws_ami.ami.id
+  image_copy_name   = var.image_copy_name
+  source_ami_region = var.region1
+  providers         = {
+    aws = aws.region4
+  }
+}
+
+module "ami_copy_region5" {
+  source            = "git::https://github.com/Chia-Network/terraform-modules.git//ami_copy"
+  source_ami_id     = data.aws_ami.ami.id
+  image_copy_name   = var.image_copy_name
+  source_ami_region = var.region1
+  providers         = {
+    aws = aws.region5
+  }
+}
